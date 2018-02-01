9cad5b79e580f6b7341f984c9efe0769d6a83b75
onur <onurkucukkece@gmail.com>
2018-02-01 23:17:52 +0300

    moved clone functions to mix task

---
+++ lib/mix/tasks/clone.ex
@@ -0,0 +1,37 @@
+defmodule Mix.Tasks.Kuroon.Clone do
+	use Mix.Task
+  alias Porcelain.Result
+  alias Porcelain.Process, as: Proc
+	@doc "start task"
+	def run([repo_from, repo_to, remote_name]) do
+		IO.puts "Cloning repository"
+    folder = SecureRandom.base64(8)
+		clone(repo_from, folder) 
+      |> remove_remote(remote_name) 
+      |> add_remote(repo_to, remote_name) 
+      |> push(remote_name)
+	end
+
+	defp clone(repo_from, folder) do
+    cmd = "git clone git@github.com:#{repo_from}.git /tmp/#{folder}"
+    result = Porcelain.shell(cmd)
+    folder
+  end
+
+  defp remove_remote(folder, remote_name) do
+    rm_remote = Porcelain.shell("cd /tmp/#{folder} && git remote rm #{remote_name}")
+    folder
+  end
+
+  defp add_remote(folder, repo_to, remote_name) do
+    add_remote = Porcelain.shell("cd /tmp/#{folder} && git remote add #{remote_name} git@github.com:#{repo_to}.git")
+    folder
+  end
+
+  defp push(folder, remote_name) do
+    push = Porcelain.shell("cd /tmp/#{folder} && git push #{remote_name} master")
+    if push.err == nil do
+      IO.puts "success" 
+    end
+  end
+end
