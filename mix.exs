08d867604d2d780ee8bd79fee2e3b924bf9ab7c0
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-02-01 23:36:48 +0100

    Do not support 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kuroon.Mixfile do
     [
       app: :kuroon,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
