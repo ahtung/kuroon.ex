41f42a2280f3c993f80e5022440f7a6e6331b9a1
onur <onurkucukkece@gmail.com>
2018-02-01 23:21:05 +0300

    readded functions

--- lib/kuroon.ex
+++ lib/kuroon.ex
@@ -1,4 +1,5 @@
 defmodule Kuroon do
+  alias Porcelain.Result
   @moduledoc """
   Documentation for Kuroon.
   """
@@ -14,5 +15,17 @@ defmodule Kuroon do
   """
   def hello do
     :world
+  end
+
+  def clone(repo_from, repo_to, remote_name) do
+    folder = SecureRandom.base64(8)
+    cmd = "git clone git@github.com:#{repo_from}.git /tmp/#{folder}"
+    result = Porcelain.shell(cmd)
+    rm_remote = Porcelain.shell("cd /tmp/#{folder} && git remote rm #{remote_name}")
+    add_remote = Porcelain.shell("cd /tmp/#{folder} && git remote add #{remote_name} git@github.com:#{repo_to}.git")
+    push = Porcelain.shell("cd /tmp/#{folder} && git push #{remote_name} master")
+    if push.err == nil do
+      IO.puts "success" 
+    end
   end
 end
