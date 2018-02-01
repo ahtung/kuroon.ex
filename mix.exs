b8f37411b5cf4dca9f063155553809ff56081eca
onur <onurkucukkece@gmail.com>
2018-02-01 20:00:15 +0300

    added secure random module

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Kuroon.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:porcelain, "~> 2.0"}
+      {:porcelain, "~> 2.0"},
+      {:secure_random, "~> 0.5.1"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
