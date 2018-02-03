1a39d3f709ab5ebc2c4ca3847b317c2fdfbf4975
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-02-03 15:25:00 +0100

    DRY up code

--- lib/kuroon.ex
+++ lib/kuroon.ex
@@ -15,11 +15,30 @@ defmodule Kuroon do
   @spec clone(binary, binary) :: binary
   def clone(repo_from, repo_to) when is_binary(repo_from) and is_binary(repo_to) do
     folder = SecureRandom.base64(8)
-    Porcelain.shell("git clone git@github.com:#{repo_from}.git /tmp/#{folder}")
-    Porcelain.shell("cd /tmp/#{folder} && git remote add tmp git@github.com:#{repo_to}.git")
-    Porcelain.shell("cd /tmp/#{folder} && git push tmp master --force")
-    Porcelain.shell("rm -rf /tmp/#{folder}")
-  
+    folder
+    |> do_clone(repo_from)
+    |> add_remote(repo_to)
+    |> push
+    clean(folder)
     :ok
+  end
+  
+  defp do_clone(folder, repo_from) do
+    "git clone git@github.com:#{repo_from}.git /tmp/#{folder}"
+    |> Porcelain.shell
+    folder
+  end
+
+  defp add_remote(folder, repo_to) do
+    Porcelain.shell("cd /tmp/#{folder} && git remote add tmp git@github.com:#{repo_to}.git")
+    folder
+  end
+
+  defp push(folder) do
+    Porcelain.shell("cd /tmp/#{folder} && git push tmp master")
+  end
+  
+  defp clean(folder) do
+    Porcelain.shell("rm -rf /tmp/#{folder}")
   end
 end
--- lib/mix/tasks/clone.ex
+++ lib/mix/tasks/clone.ex
@@ -5,38 +5,7 @@ defmodule Mix.Tasks.Kuroon.Clone do
   alias Porcelain.Result
 	
   @doc "start task"
-	def run([repo_from, repo_to, remote_name]) do
-    folder = SecureRandom.base64(8)
-		clone(repo_from, folder) 
-      |> remove_remote(remote_name) 
-      |> add_remote(repo_to, remote_name) 
-      |> push(remote_name)
+	def run([repo_from, repo_to]) do
+		Kuroon.clone(repo_from, repo_to)
 	end
-
-	defp clone(repo_from, folder) do
-    IO.puts "Cloning repository"
-    cmd = "git clone git@github.com:#{repo_from}.git /tmp/#{folder}"
-    result = Porcelain.shell(cmd)
-    folder
-  end
-
-  defp remove_remote(folder, remote_name) do
-    IO.puts "Removing old remote"
-    rm_remote = Porcelain.shell("cd /tmp/#{folder} && git remote rm #{remote_name}")
-    folder
-  end
-
-  defp add_remote(folder, repo_to, remote_name) do
-    IO.puts "Adding new remote"
-    add_remote = Porcelain.shell("cd /tmp/#{folder} && git remote add #{remote_name} git@github.com:#{repo_to}.git")
-    folder
-  end
-
-  defp push(folder, remote_name) do
-    IO.puts "Pushing repository to new remote"
-    push = Porcelain.shell("cd /tmp/#{folder} && git push #{remote_name} master")
-    if push.err == nil do
-      IO.puts "success" 
-    end
-  end
 end
